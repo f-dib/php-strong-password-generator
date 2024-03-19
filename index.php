3d85e2b36b5aed1ebdd8e6e0468106604d314f08
federico-dibitonto <f.dibitonto94@gmail.com>
2024-03-19 17:08:08 +0100

    milestone 1 completed

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+
+    function rand_string( $passLength ) {
+
+        $allChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return substr(str_shuffle($allChars),0,$passLength);
+        
+    }
+        
+    $numChars = $_GET['number'];
+    
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +20,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Generatore di Password</title>
+    <link rel="icon" type="image/x-icon" href="./img/unnamed.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     
+    <div class="container-xxl py-5 text-center">
+        <h1 class="text-primary mb-3">Password Generator</h1>
+        <form class="mb-5">
+            <label for="numberInput">Genera Password da 8 a 20 caratteri:</label>
+            <input type="number" id="numberInput" name="number" min="8" max="20" required><br><br>
+            <input type="submit" value="Genera">
+        </form>
+        <div>
+            <?php
+            echo rand_string($numChars);
+            ?>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
