4fdbe90ce7a56adda07a4ed58ec971adc12b9db1
federico-dibitonto <f.dibitonto94@gmail.com>
2024-03-19 16:01:45 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generatore di Password</title>
+</head>
+<body>
+    
+</body>
+</html>
